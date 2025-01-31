5ddcf8393fd30f1fcf54e2b09f5ffc173f938be3
louisheal <louisheal@live.co.uk>
2025-01-31 18:37:05 +0000

    remove unused css

--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -8,8 +8,8 @@ function Dashboard({ overlayId, channelId }: DashboardProps) {
         <>
             <h1>Dashboard</h1>
             <h2>
-                https://overlay.streampets.io?overlayId={overlayId}&channelId=
-                {channelId}
+                https://overlay.streampets.io?overlayId={overlayId}
+                &channelId={channelId}
             </h2>
         </>
     );
